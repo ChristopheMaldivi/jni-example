39e863829cd458a268590719990ba5f18adbf58b
Christophe Maldivi <christophe.maldivi@orange.com>
2020-12-10 11:30:35 +0100

    update native build script for linux

--- native-library/build-linux.sh
+++ native-library/build-linux.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+JAVA_HOME=$(java -XshowSettings:properties -version 2>&1 > /dev/null | awk '/java.home/ {print $3}')
+echo JAVA_HOME=$JAVA_HOME
+echo
+
 rm -f *.o *.so
-gcc -c dothejob.c | exit 1
-gcc -shared dothejob.o -o libdothejob.so | exit 2
+gcc -I $JAVA_HOME/include -I $JAVA_HOME/include/linux -Wall -c dothejob.c JNI_DoTheJob.c | exit 1
+gcc -shared dothejob.o JNI_DoTheJob.o -o libdothejob.so | exit 2
 cp libdothejob.so ../src/main/resources | exit 3
 rm -f *.o *.so
 
